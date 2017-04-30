97bc65804c36caae2611c900bc44a28b4976a7a6
Adrian Bobrowski <adrian071993@gmail.com>
2017-04-30 15:14:35 +0200

    Localizable.strings

--- LocalizationStringsFileDemo/ViewController.swift
+++ LocalizationStringsFileDemo/ViewController.swift
@@ -10,16 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var breadLabel: UILabel!
+    @IBOutlet weak var tomatoLabel: UILabel!
+    @IBOutlet weak var cucumberLabel: UILabel!
+    
+    @IBOutlet weak var breadAmountLabel: UILabel!
+    @IBOutlet weak var tomatoAmountLabel: UILabel!
+    @IBOutlet weak var cucumberAmountLabel: UILabel!
+    
+    private var language = "pl"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.l10n()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func l10n() {
+        let bundle = self.createBundle(for: self.language)!
+        self.title = bundle.localizedString(forKey: "ShoppingList", value: nil, table: nil)
+        self.breadLabel.text = bundle.localizedString(forKey: "Bread", value: nil, table: nil)
+        self.tomatoLabel.text = bundle.localizedString(forKey: "Tomato", value: nil, table: nil)
+        self.cucumberLabel.text = bundle.localizedString(forKey: "Cucumber", value: nil, table: nil)
     }
-
-
+    
+    func createBundle(for language: String) -> Bundle? {
+        guard let path = Bundle.main.path(forResource: language, ofType: "lproj") else {
+            return nil
+        }
+        return Bundle(path: path)
+    }
+    
+    @IBAction func onBreadAmountValueChanged(_ sender: UIStepper) {
+        self.breadAmountLabel.text = Int(sender.value).description
+    }
+    
+    @IBAction func onTomatoAmountValueChanged(_ sender: UIStepper) {
+        self.tomatoAmountLabel.text = Int(sender.value).description
+    }
+    
+    @IBAction func onCucumberAmountValueChanged(_ sender: UIStepper) {
+        self.cucumberAmountLabel.text = Int(sender.value).description
+    }
+    
+    @IBAction func onLanguageChanged(_ sender: UISegmentedControl) {
+        self.language = ["pl", "en"][sender.selectedSegmentIndex]
+        self.l10n()
+    }
 }
 
